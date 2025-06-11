1bcf7f34efaf6250445000db706625bf4be00b38
RadyaAlbasha <radya.albasha15@gmail.com>
2025-06-11 21:42:58 +0300

    create button and refactoring

--- Weather-SwiftUI/ContentView.swift
+++ Weather-SwiftUI/ContentView.swift
@@ -10,28 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, Color("lightBlue")]),
-                           startPoint: .topLeading,
-                           endPoint: .bottomTrailing)
-            .ignoresSafeArea()
+            BackgroundView(topColor: .blue, bottomColor: Color("lightBlue"))
             
             VStack{
-                Text("Cupretino, CA")
-                    .font(.system(size: 32, weight: .medium, design: .default))
-                    .foregroundColor(.white)
-                    .padding()
+                CityTextView(cityName: "Cupretino, CA")
                 
-                VStack(spacing: 10){
-                  Image(systemName:  "cloud.sun.fill")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 180, height: 180)
-                    Text("76°")
-                        .font(.system(size: 70, weight: .medium))
-                        .foregroundStyle(.white)
-                }
-                .padding(.bottom, 40)
+               MainWeatherStatusView(imageName: "cloud.sun.fill", temprature: 76)
                 
                 HStack(spacing: 20){
                     WeatherDayView(dayOfWeek: "TUE",
@@ -53,6 +37,13 @@ struct ContentView: View {
                 }
                 
                 Spacer()
+                
+                Button{//action
+                    print("Tapped")
+                }label: {
+                    WeatherButton(title: "Change Day Time", TextColor: .blue, backgroundColor: .white)
+                }
+                Spacer()
             }
         }
              
@@ -64,9 +55,11 @@ struct ContentView: View {
 }
 
 struct WeatherDayView: View {
+    
     var dayOfWeek:  String
     var imageName: String
     var temprature: Int
+    
     var body: some View {
         VStack{
             Text(dayOfWeek)
@@ -83,3 +76,49 @@ struct WeatherDayView: View {
         }
     }
 }
+
+struct BackgroundView: View {
+    
+    var topColor: Color
+    var bottomColor: Color
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [topColor, bottomColor ]),
+                       startPoint: .topLeading,
+                       endPoint: .bottomTrailing)
+        .ignoresSafeArea()
+    }
+}
+
+struct CityTextView: View {
+    
+    var cityName: String
+    
+    var body: some View {
+        Text(cityName)
+            .font(.system(size: 32, weight: .medium))
+            .foregroundColor(.white)
+            .padding()
+    }
+}
+
+struct MainWeatherStatusView: View {
+    
+    var imageName: String
+    var temprature: Int
+    
+    var body: some View {
+        VStack(spacing: 10){
+          Image(systemName:  imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 180, height: 180)
+            Text("\(temprature)°")
+                .font(.system(size: 70, weight: .medium))
+                .foregroundStyle(.white)
+        }
+         .padding(.bottom, 40)
+    }
+}
+
---
+++ Weather-SwiftUI/WeatherButton.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherButton.swift
+//  Weather-SwiftUI
+//
+//  Created by Radya Albasha on 11/06/2025.
+//
+
+import SwiftUI
+
+struct WeatherButton: View {
+    
+    var title: String
+    var TextColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 280 , height: 50)
+            .background(backgroundColor)
+            .foregroundStyle(TextColor)
+            .font(.system(size: 20, weight: .bold))
+            .cornerRadius(10)
+    }
+}
